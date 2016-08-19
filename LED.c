a919c5565a279f7e05a001d5124a39aeb2fc1a2d
vidisha15189 <vidisha15189@iiitd.ac.in>
2016-08-19 21:22:20 +0530

    Add files via upload

---
+++ LED.c
@@ -0,0 +1,19 @@
+#ifndef F_CPU
+#define F_CPU 16000000UL         // 16 MHz clock speed
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRC = 0xFF;                //Nakes PORTB as Output
+    while(1)                    //infinite loop
+    {
+        PORTB = 0xFF;           //Turns ON All LEDs
+        _delay_ms(10000);       //1 second delay
+        PORTB= 0x00;            //Turns OFF All LEDs
+        _delay_ms(1000);        //1 second delay
+    }
+}
+
---
+++ makefile
@@ -0,0 +1,20 @@
+#vidisha(2015189) and bhavna(2015136)
+
+all: compile program
+
+compile:
+	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o LED.o LED.c
+
+	avr-gcc -mmcu=atmega328p LED.o -o LED.out
+
+	avr-objcopy -O ihex LED.out LED.hex
+
+	
+program:
+	sudo avrdude -c avrisp2 -p ATMEGA328P -P /dev/ttyUSB* -U flash:w:led.hex
+
+
+clean:
+	-rm -rf *.o *.out *.hex
+
+	
